a8781a83347b9720f75f085cf899dc075f4e606c
GokulAthi <goku2812@gmail.com>
2020-08-16 17:39:48 +0530

    Take screenshot of element file

---
+++ src/test/java/TakeScreenshotOfElementUsingSelenium4.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class TakeScreenshotOfElementUsingSelenium4 {
+
+    public static void main(String[] args) throws IOException {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement logo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("hplogo")));
+        File file = logo.getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(file, new File(System.getProperty("user.dir")+"/src/test/downloads/logo.png"));
+
+        driver.quit();
+    }
+}
